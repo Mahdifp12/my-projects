2b96eb35f8f0e43e48bd29cc4533cdb4042df742
mhd <mahdifallahtpishe80@gmail.com>
2021-12-20 01:26:52 +0330

    Edited calculate_fibonacci.py file

--- calculate_fibonacci.py
+++ calculate_fibonacci.py
@@ -1,6 +1,6 @@
-def calc_fibonacci(n):
-    if n in [0, 1]:
-        return n
-    return calc_fibonacci(n - 1) + calc_fibonacci(n - 2)
+def calc_fibonacci(num):
+    if num in [0, 1]:
+        return num
+    return calc_fibonacci(num - 1) + calc_fibonacci(num - 2)
 
 print([calc_fibonacci(n) for n in range(6)])
